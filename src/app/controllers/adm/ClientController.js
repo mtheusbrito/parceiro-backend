79dfdbd6ac859190589f108494d96095d96016ed
mtheusbrito <matheusbritodasilva@gmail.com>
2021-02-13 00:54:16 -0300

    updating address clients

--- src/app/controllers/adm/ClientController.js
+++ src/app/controllers/adm/ClientController.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-console */
 import * as Yup from 'yup';
-import Sequelize from 'sequelize';
 import Address from '../../models/Address';
 import Client from '../../models/Client';
 import User from '../../models/User';
-import databaseConfig from '../../../config/database';
-
-const { QueryTypes } = require('sequelize');
-
-const sequelize = new Sequelize(databaseConfig);
 
 class ClientController {
   async index(req, res) {
@@ -79,9 +73,6 @@ class ClientController {
         )
         .required(),
     });
-    console.log(
-      '--------------------------------------------------------------------------------'
-    );
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validadion fails' });
     }
@@ -90,79 +81,74 @@ class ClientController {
     if (!user_database) {
       return res.status(404).json({ error: 'Este usuário não existe. ' });
     }
+    // const { id, name, cnpj, company, obs, user_id } = ;
+
+    const client_database = await Client.findByPk(req.body.id, {
+      include: { model: Address, as: 'addresses' },
+    });
+    const addresses_request = req.body.addresses;
+    const addresses_database = await client_database.getAddresses();
+
+    await Promise.all(
+      addresses_database.map(async (address) => {
+        const deleted = addresses_request.find((a) => a.id === address.id);
+        if (!deleted) {
+          await client_database.removeAddress(address);
+        }
+        return deleted;
+      })
+    );
+    await Promise.all(
+      addresses_request.map(async (address) => {
+        if (address.id !== null && address.id !== undefined) {
+          const address_exist = await Address.findByPk(address.id);
+          if (address_exist) {
+            const {
+              name,
+              city,
+              number,
+              state_registration,
+              complement,
+              google_maps,
+            } = address;
+            await address_exist.update({
+              name,
+              city,
+              number,
+              state_registration,
+              complement,
+              google_maps,
+            });
+          }
+        } else {
+          await client_database.createAddress(address);
+        }
+      })
+    );
     const { id, name, cnpj, company, obs, user_id } = req.body;
-    const client_database = await Client.findByPk(id, {
+
+    await client_database.update({
+      id,
+      name,
+      cnpj,
+      company,
+      obs,
+      user_id,
+    });
+
+    const client_response = await Client.findByPk(id, {
       include: { model: Address, as: 'addresses' },
     });
 
-    if (!client_database) {
-      return res.status(404).json({ error: 'Este cliente não existe. ' });
-    }
+    return res.json(client_response);
 
-    async function removeAddress(address) {
-      console.log(`a ser removido${address.id}`);
-      await Address.destroy({ where: { id: address.id } });
-    }
-    async function createOrUpdate(address, client_id) {
-      // a ser criado out atualizado
-      console.log(`a ser criado out atualizado${address.id}`);
-
-      try {
-        const address_database = await Address.findByPk(address.id);
-        if (address_database) {
-          address_database.update(address);
-        } else {
-          const response = await Address.create(address);
-          if (response) {
-            // const { address_id } = response.id;
-
-            const address_id = response.id;
-            const address_client = await sequelize.query(
-              `INSERT INTO \`address_clients\` (\`client_id\`,\`address_id\`,\`created_at\`,\`updated_at\`) VALUES (${client_id},${address_id},now(),now());`,
-              {
-                type: QueryTypes.INSERT,
-              }
-            );
-            if (address_client) {
-              console.log('persistiu');
-            }
-          }
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    const addresses_request = req.body.addresses;
-    const addresses_database = client_database.addresses;
-    // pesquisando os enderecos do banco e verificando se estao na query de update, caso
-    // não esteja signnifica que o usuario removeu
-    if (addresses_database !== null && addresses_database.length > 0) {
-      addresses_database.forEach((address) => {
-        const resp = addresses_request.find((a) => a.id === address.id);
-        // console.log(resp);
-        if (!resp) {
-          removeAddress(address);
-        }
-      });
-    }
-    addresses_request.forEach((address) => {
-      createOrUpdate(address, client_database.id);
-    });
-
-    // return res.json(addresses_request);
-
-    const client = await client_database.update(
-      { id, name, cnpj, company, obs, user_id },
-      {
-        include: [
-          {
-            model: Address,
-            as: 'addresses',
-          },
-        ],
-      }
-    );
-    return res.json(client);
+    // await Promise.all(
+    //   addresses.map(async (address) => {
+    //     const results = await Address.create(address);
+    //     const response = await client.addAddress(results);
+    //     return response;
+    //   })
+    // );
   }
 }
 export default new ClientController();
