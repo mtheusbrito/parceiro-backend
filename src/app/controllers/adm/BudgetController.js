a117824b23f2a20f95394349f99f9d285b65d98a
mtheusbrito <matheusbritodasilva@gmail.com.br>
2021-03-17 11:59:47 -0300

    retornando url

--- src/app/controllers/adm/BudgetController.js
+++ src/app/controllers/adm/BudgetController.js
@@ -33,10 +33,8 @@ class BudgetController {
       fileName: 'servicesBudget.ejs',
       data: budget,
     });
-
-    return res.json({
-      urlDownload: `${process.env.URL_API}reports/${budget.hash}.pdf`,
-    });
+    const urlDownload = `${process.env.URL_API}reports/${budget.hash}.pdf`;
+    return res.json({ urlDownload });
   }
 
   static async sendEmailBudgetUpdated(budget_updated) {
